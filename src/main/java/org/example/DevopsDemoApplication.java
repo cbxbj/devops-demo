368a455b66aa6ca5e73eedef0ee561ccb6a65322
jiayk <jiayk@tuang.com>
2024-02-04 09:57:26 +0800

    Initial commit

---
+++ src/main/java/org/example/DevopsDemoApplication.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsDemoApplication.class, args);
+    }
+}
